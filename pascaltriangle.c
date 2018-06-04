fa2c56db723f91583942f536537cfa895bf44727
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2018-06-04 23:32:18 +0300

    Code refactor

--- pascaltriangle.c
+++ pascaltriangle.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 
-int factorial(int number){
-  if (number == 0)
-    return 1;
-  else
-    return(number * factorial(number-1));
+void ptriangle(int rows);
+int binomial(int number, int choose);
+int factorial(int number);
+
+int main(void){
+	int rows;
+
+	printf("Rows of the triangle: ");
+	scanf("%d", &rows);
+	ptriangle(rows);
+
+	return 0;
+}
+
+void ptriangle(int rows){
+	int number,spaces,choose;
+	for(number=0; number <= rows; number++){
+		for(spaces = rows; spaces >= number; spaces--){
+			printf(" ");
+		}
+		for(choose = 0; choose <= number; choose++){
+		printf("%d ",binomial(number,choose));
+		}
+		printf("\n");
+	}
 }
 
 int binomial(int number, int choose){
-    if (number == 0)
-        return 1;
-    else
-        return factorial(number)/(factorial(choose)*factorial(number-choose));
+	if (number == 0)
+		return 1;
+	else
+		return factorial(number)/(factorial(choose)*factorial(number-choose));
 }
 
-void ptriangle(int number){
-    int i,spaces,choose;
-    for(i=0; i <= number; i++){
-        for(spaces = number; spaces >= i; spaces--){
-            printf(" ");
-        }
-        for(choose = 0; choose <= i; choose++){
-        printf("%d ",binomial(i,choose));
-        }
-        printf("\n");
-    }
-}
-
-int main(void){
-    int number,choose;
-    printf("Give me a number: ");
-    scanf("%d", &number);
-    ptriangle(number);
-
-    return 0;
+int factorial(int number){
+  if (number == 0)
+	return 1;
+  else
+	return(number * factorial(number-1));
 }
