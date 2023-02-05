094fb27163eb41c9b8ae06a944358ad5d32296d2
Constantinos Georgiou <constantgeorgiou@gmail.com>
2023-02-05 18:20:42 +0200

    Add Makefile experiments

---
+++ c/makefiles/dry/src/coffee.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // printf()
+
+int
+main (int argc, char const *argv[])
+{
+  printf ("Alpha\n");
+  return 0;
+}
---
+++ c/makefiles/dry/src/tea.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // printf()
+
+int
+main (int argc, char const *argv[])
+{
+  printf ("Delta\n");
+  return 0;
+}
